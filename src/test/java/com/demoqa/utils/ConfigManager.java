e832d0844f0e90178aacfb7acc61f5a1acdae464
Paulo Roberto <evans_sp@hotmail.com>
2025-08-23 16:35:49 -0300

    Refactor ConfigManager

--- src/test/java/com/demoqa/utils/ConfigManager.java
+++ src/test/java/com/demoqa/utils/ConfigManager.java
@@ -1,4 +1,25 @@
 package com.demoqa.utils;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class ConfigManager {
+    private static final Properties props = new Properties();
+
+    static {
+        try (InputStream in = ConfigManager.class.getClassLoader()
+                .getResourceAsStream("config.properties")) {
+            if (in == null) {
+                throw new IllegalStateException("config.properties não encontrado em test/resources");
+            }
+            props.load(in);
+        } catch (IOException e) {
+            throw new RuntimeException("Falha ao carregar config.properties", e);
+        }
+    }
+
+    public static String get(String key) {
+        return props.getProperty(key);
+    }
 }
