5ca71d488efd02b30d0ccae52e1349441b3ce3dc
Paulo Roberto <evans_sp@hotmail.com>
2025-08-25 23:09:12 -0300

    Refactor report

--- src/test/java/com/demoqa/TestSuite.java
+++ src/test/java/com/demoqa/TestSuite.java
@@ -1,9 +1,10 @@
 package com.demoqa;
 
+import com.demoqa.tests.BookApiTest;
 import com.demoqa.tests.SmokeFlowTest;
 import com.demoqa.tests.UserApiTest;
-import com.demoqa.tests.BookApiTest;
-
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
@@ -20,10 +21,21 @@ import org.slf4j.LoggerFactory;
 public class TestSuite {
 
     private static final Logger log = LoggerFactory.getLogger(TestSuite.class);
+    public static ExtentReports extent;
 
     static {
         log.info("==========================================");
         log.info("INICIANDO A EXECUÇÃO DA BOOKSTORE API SUITE");
         log.info("==========================================");
+
+        ExtentSparkReporter spark = new ExtentSparkReporter("target/extent-report.html");
+        extent = new ExtentReports();
+        extent.attachReporter(spark);
+    }
+
+    public static void flushReport() {
+        if (extent != null) {
+            extent.flush();
+        }
     }
 }
